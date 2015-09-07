14bd260d58a24249d87d4f458779b9bce5a5577f
mudasar <mudassar.rauf@gmail.com>
2015-09-08 00:57:44 +0100

    callback class

---
+++ app/src/main/java/uk/appinvent/popularmovies/ClickCallback.java
@@ -0,0 +1,11 @@
+package uk.appinvent.popularmovies;
+
+import android.net.Uri;
+
+/**
+ * Created by mudasar on 06/09/15.
+ */
+public interface ClickCallback {
+
+    public void onGridItemSelected(Uri uri);
+}
